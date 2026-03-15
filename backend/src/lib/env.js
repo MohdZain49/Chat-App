b73fbf50bbf37df300f041150d892e738301d6b3
Mohd Zain <mohammedzain4099@gmail.com>
2026-03-15 14:37:13 +0530

    feat: auth middleware and cloudinary config

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -4,13 +4,21 @@ dotenv.config();
 
 const _env = {
   PORT: process.env.PORT,
-  MONGO_DB_URI: process.env.MONGO_DB_URI,
   NODE_ENV: process.env.NODE_ENV,
+
+  MONGO_DB_URI: process.env.MONGO_DB_URI,
+  CLIENT_URL: process.env.CLIENT_URL,
+
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+
   RESEND_API_KEY: process.env.RESEND_API_KEY,
   RESEND_FROM: process.env.RESEND_FROM,
   RESEND_FROM_NAME: process.env.RESEND_FROM_NAME,
-  CLIENT_URL: process.env.CLIENT_URL,
+
+  CLOUDINARY_CLOUD_NAME: process.env.CLIENT_URL,
+  CLOUDINARY_FOLDER_NAME: process.env.CLIENT_URL,
+  CLOUDINARY_API_KEY: process.env.CLIENT_URL,
+  CLOUDINARY_API_SECRET: process.env.CLIENT_URL,
 };
 
 export const ENV = Object.freeze(_env);
---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+import { ENV } from "../lib/env.js";
+
+const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized - No token provided" });
+    }
+
+    const decodedToken = jwt.verify(token, ENV.JWT_SECRET_KEY);
+    if (!decodedToken) {
+      return res.status(401).json({ message: "Unauthorized - Invalid token" });
+    }
+
+    const user = await User.findById(decodedToken.userId).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error("Error in protectRoute middleware", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export default protectRoute;
