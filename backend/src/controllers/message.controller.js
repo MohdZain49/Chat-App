9b371998ca9ffe2b43e2f85c8733214bdba123f6
Mohd Zain <mohammedzain4099@gmail.com>
2026-03-16 17:08:57 +0530

    feat: implement get messages and send message endpoint

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,12 +1,15 @@
 import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
+
+import cloudinary from "../lib/cloudinary.js";
 
 export const getAllContacts = async (req, res) => {
   try {
     const loggedInUser = req.user._id;
 
-    const filteredUsers = await User.find({ _id: { $ne: loggedInUser } }).select(
-      "-password",
-    );
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUser },
+    }).select("-password");
 
     if (!filteredUsers) {
       return res.status(404).json({
@@ -22,3 +25,66 @@ export const getAllContacts = async (req, res) => {
     });
   }
 };
+
+export const getMessagesByUserId = async (req, res) => {
+  try {
+    const myId = req.user._id;
+    const { id } = req.params;
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: id },
+        { senderId: id, receiverId: myId },
+      ],
+    });
+
+    if (!messages) {
+      return res.status(404).json({
+        message: "messages not found",
+      });
+    }
+
+    return res.status(200).json(messages);
+  } catch (error) {
+    console.error("Error in 'get all contact' controller:", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const senderId = req.user._id;
+    const receiverId = req.params.id;
+
+    const { text, image } = req.body;
+    if (!text && !image) {
+      return res.status(400).json({
+        message: "content of message required",
+      });
+    }
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = await Message.create({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    // todo: send message in real-time if user is online - socket.io
+
+    return res.status(201).json(newMessage);
+  } catch (error) {
+    console.error("Error in send message controller:", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+  
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,17 @@
 import express from "express"
 import protectRoute from "../middlewares/auth.middleware.js";
-import { getAllContacts } from "../controllers/message.controller.js";
+import {
+  getAllContacts,
+  getMessagesByUserId,
+  sendMessage,
+} from "../controllers/message.controller.js";
 
 const messageRouter = express.Router()
 
 messageRouter.get("/contacts", protectRoute, getAllContacts);
+
+messageRouter.get("/:id", protectRoute, getMessagesByUserId);
+
+messageRouter.post("/send/:id", protectRoute, sendMessage)
 
 export default messageRouter;
