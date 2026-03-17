71c2809c58b575756cc5fc24dd1af639762f0ae6
Mohd Zain <mohammedzain4099@gmail.com>
2026-03-17 16:51:55 +0530

    create initial frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-red-300">
+      App
+      <button className="btn btn-primary">click</button>
+    </div>
+  );
+}
+
+export default App;
