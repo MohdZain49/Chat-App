a9c3be8419e554d74cff6b762eb494b80ed472ee
Mohd Zain <mohammedzain4099@gmail.com>
2026-03-18 16:21:37 +0530

    feat: implement signup page and its functionaliy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,23 @@
-import React from "react";
-import { Routes, Route } from "react-router";
+import React, { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router";
 import ChatPage from "./pages/ChatPage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import useAuthStore from "./store/useAuthStore.js";
+import PageLoader from "./components/PageLoader.jsx";
 
 function App() {
-  const { authUser, isLoding, isLoggedIn, login } = useAuthStore();
+  const { authUser, isCheckingAuth, checkAuth } = useAuthStore();
 
-  console.log("name", authUser);
-  console.log("lodding : ", isLoding);
-  console.log("isloggedIn : ", isLoggedIn)
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log("user : ", authUser);
+
+  if (isCheckingAuth) {
+    return <PageLoader />;
+  }
 
   return (
     <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
@@ -19,11 +26,19 @@ function App() {
       <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
       <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
 
-      <button onClick={login} className="z-10 bg-gray-600 p-3 m-4 border-2 border-blue-800 rounded-2xl" >Login</button>
       <Routes>
-        <Route path={"/"} element={<ChatPage />} />
-        <Route path={"/signup"} element={<SignUpPage />} />
-        <Route path={"/login"} element={<LoginPage />} />
+        <Route
+          path={"/"}
+          element={authUser ? <ChatPage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path={"/signup"}
+          element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path={"/login"}
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+        />
       </Routes>
     </div>
   );
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,13 +1,35 @@
 import { create } from "zustand";
+import axiosInstance from "../lib/axios";
+import toast from "react-hot-toast";
 
 const useAuthStore = create((set) => ({
-  authUser: { _id: "2a3238kse134", name: "Mohd Zain", age: 24 },
-  isLoding: true,
-  isLoggedIn: false,
+  authUser: null,
+  isCheckingAuth: true,
+  isSigninUp: false,
 
-  login: () => {
-    console.log("user logged in successfully");
-    set({ isLoggedIn: true, isLoding: false });
+  signUp: async (userData) => {
+    set({ isSigninUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", userData);
+      set({ authUser: res.data });
+      toast.success("Account created successfully!");
+    } catch (error) {
+      console.error("Error in signup auth:\n", error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isSigninUp: false });
+    }
+  },
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.error("Error in check auth:\n", error);
+    } finally {
+      set({ isCheckingAuth: false });
+    }
   },
 }));
 
