1edae33662cf25a93ac15268933c6d42034cfe38
Mohd Zain <mohammedzain4099@gmail.com>
2026-03-30 11:41:16 +0530

    fix: correct Cloudinary environment variable assignments and enhance JSON parsing limits

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,13 +13,17 @@ import messageRouter from "./routes/message.route.js";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" })); 
+
+app.use(express.urlencoded({ extended: true }));
+
 app.use(
   cors({
     origin: ENV.CLIENT_URL,
     credentials: true,
   }),
 );
+
 app.use(cookieParser());
 
 app.use(arcjetProtection);
