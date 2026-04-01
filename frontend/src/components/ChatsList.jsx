3e59718d3e306e0497628adc698aa8a8ddd98ea5
Mohd Zain <mohammedzain4099@gmail.com>
2026-04-01 10:45:16 +0530

    feat: implement ChatsList and ContactList components with loading and no chat states feat: add NoChatsFound and UsersLoadingSkeleton components for better user experience

--- frontend/src/components/ChatsList.jsx
+++ frontend/src/components/ChatsList.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import useChatStore from "../store/useChatStore";
+import UserLoadingSkeleton from "../components/UsersLoadingSkeleton";
+import NoChatsFound from "../components/NoChatsFound";
 
 function ChatsList() {
+  const { getChatPartners, chats, isUsersLoading, setSelectedUser } =
+    useChatStore();
+
+  useEffect(() => {
+    getChatPartners();
+  }, [getChatPartners]);
+
+  if (isUsersLoading) return <UserLoadingSkeleton />;
+  if (chats.length === 0) return <NoChatsFound />;
   return (
-    <div>ChatsList</div>
-  )
+    <>
+      {chats.map((chat) => (
+        <div
+          key={chat._id}
+          className="bg-cyan-500/10 p-4 rounded-lg cursor-pointer hover:bg-cyan-500/20 transition-colors"
+          onClick={() => setSelectedUser(chat)}
+        >
+          <div className="flex items-center gap-3">
+            <div
+              className={`avatar avatar-online`}
+            >
+              <div className="size-12 rounded-full">
+                <img
+                  src={chat.profilePic || "/avatar.png"}
+                  alt={chat.fullName}
+                />
+              </div>
+            </div>
+            <h4 className="text-slate-200 font-medium truncate">
+              {chat.fullName}
+            </h4>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }
 
-export default ChatsList
+export default ChatsList;
